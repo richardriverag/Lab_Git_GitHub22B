f9913027de711cb7ea7337ececed19cc39397cb7
Daniel Mera <danielfernansdo404@gmail.com>
2023-01-02 20:49:31 -0500

    agregar saludo y despedida

---
+++ archivo2.cpp
@@ -0,0 +1,15 @@
+# include<iostream>
+# include<string>
+
+using namespace std;
+
+int main(){
+
+    string name;
+    cout<<"ingrese su nombre: "<<endl;
+    getline(cin,name);
+    cout<<endl;
+    cout<<"bienvenido: "<<name;
+
+    return 0;
+}
